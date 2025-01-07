e1165d8bab08d95252ccecd4d714c12312a39771
Henrique Guimarães <henrique.guimaraes@ati.com.br>
2025-01-07 16:54:56 -0300

    feat: :sparkles: adds db option file input to server

--- server/src/index.js
+++ server/src/index.js
@@ -1,27 +1,30 @@
 const cors = require("cors");
 const express = require("express");
 const mysql = require("mysql2");
+const fs = require("node:fs");
 
 let dateLimits;
 
 const app = express();
 const port = 3001;
 
-const dbHost = "localhost";
+let rawdata = fs.readFileSync("./src/dbopts.json");
+let dbOpts = JSON.parse(rawdata);
+
 const connection = mysql.createPool({
-  host: dbHost,
-  user: "hngui",
-  password: "cube",
-  database: "paystubs",
-  port: 3306,
+  host: dbOpts.host,
+  user: dbOpts.user,
+  password: dbOpts.password,
+  database: dbOpts.database,
+  port: dbOpts.port,
   connectionLimit: 10,
 });
 
 const getStartAndEndDates = async () => {
   const query = `
     SELECT 
-      CONCAT(MIN(CONCAT(year, '-', LPAD(month, 2, '0')))) AS minDate, 
-      CONCAT(MAX(CONCAT(year, '-', LPAD(month, 2, '0')))) AS maxDate 
+      MIN(timestamp) AS minTimestamp, 
+      MAX(timestamp) AS maxTimestamp 
     FROM entries
   `;
   const dates = await new Promise((resolve, reject) => {
@@ -29,14 +32,14 @@ const getStartAndEndDates = async () => {
       if (error) {
         reject(error);
       } else {
-        const minDateParts = results[0].minDate.split("-");
-        const maxDateParts = results[0].maxDate.split("-");
+        const minDate = new Date(results[0].minTimestamp * 1000);
+        const maxDate = new Date(results[0].maxTimestamp * 1000);
 
         const out = {};
-        out.minYear = parseInt(minDateParts[0], 10);
-        out.minMonth = parseInt(minDateParts[1], 10);
-        out.maxYear = parseInt(maxDateParts[0], 10);
-        out.maxMonth = parseInt(maxDateParts[1], 10);
+        out.minYear = minDate.getFullYear();
+        out.minMonth = minDate.getMonth() + 1;
+        out.maxYear = maxDate.getFullYear();
+        out.maxMonth = maxDate.getMonth() + 1;
         resolve(out);
       }
     });
@@ -60,23 +63,77 @@ app.get("/entries", async (req, res) => {
 
   const { month, year } = req.query;
 
-  try {
-    const query = "SELECT * FROM entries WHERE MONTH = ? AND YEAR = ?";
-    const results = await new Promise((resolve, reject) => {
-      connection.query(query, [month, year], (error, results) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(results);
-        }
-      });
-    });
+  if (
+    !month ||
+    !year ||
+    isNaN(month) ||
+    isNaN(year) ||
+    month < 1 ||
+    month > 12 ||
+    year < 2000 ||
+    year > new Date().getFullYear()
+  ) {
+    return res.status(400).send("Invalid month or year");
+  }
 
+  try {
+    const startDate = new Date(year, month - 1, 1);
+    const endDate = new Date(year, month, 0, 23, 59, 59);
+    const query = "SELECT * FROM entries WHERE timestamp BETWEEN ? AND ?";
+    const results = await new Promise((resolve, reject) => {
+      connection.query(
+        query,
+        [startDate.getTime() / 1000, endDate.getTime() / 1000],
+        (error, results) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(results);
+          }
+        }
+      );
+    });
     res.json(results);
   } catch (error) {
     res.status(500).send(error);
   }
 });
+
+// app.post("/entries", async (req, res) => {
+//   console.log(req.query);
+
+//   const { month, year, code, description, value, group } = req.query;
+
+//   try {
+//     const query = `
+//         INSERT INTO entries (month, year, code, description, value, group)
+//         VALUES (?, ?, ?, ?, ?, ?)
+//         ON DUPLICATE KEY UPDATE
+//           description=VALUES(description),
+//           value=VALUES(value)
+//           group=VALUES(group)
+//       `;
+
+//     // const query = "SELECT * FROM entries WHERE MONTH = ? AND YEAR = ?";
+//     const results = await new Promise((resolve, reject) => {
+//       connection.query(
+//         query,
+//         [month, year, code, description, value, group],
+//         (error, results) => {
+//           if (error) {
+//             reject(error);
+//           } else {
+//             resolve(results);
+//           }
+//         }
+//       );
+//     });
+
+//     res.json(results);
+//   } catch (error) {
+//     res.status(500).send(error);
+//   }
+// });
 
 app.get("/datelimits", (req, res) => {
   res.json(dateLimits);
